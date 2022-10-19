dcf45aa19e3e71bf62f19861bf52ebe3b8cb14ac
Elena <lenalosyakova@gmail.com>
2022-10-19 21:56:47 +0300

    x и y

--- work1/Program.cs
+++ work1/Program.cs
@@ -8,9 +8,7 @@ int b2 = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("Введите число k2");
 int k2 = int.Parse(Console.ReadLine() ?? "0");
 double x = (b2 - b1) / (k1 - k2);
-//x = int.Parse(Console.ReadLine() ?? "0");
 double y = k2 * ((b2 - b1) / (k1 - k2)) + b2;
-//y = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine("x = " + x);
 Console.WriteLine("y = " + y);
 
